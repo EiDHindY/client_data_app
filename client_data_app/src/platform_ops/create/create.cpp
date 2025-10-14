055cfd2617f6606bd484a922a056fb235f2589dc
EiD HindY <eidabdelwahabhindy.gmail.com>
2025-10-14 19:44:10 +0300

    replaced helpers with create and moved create_data_dir() to create and get_exe() to paths

---
+++ client_data_app/src/platform_ops/create/create.cpp
@@ -0,0 +1,14 @@
+﻿// platform_ops/create.cpp
+#include "create.h"
+
+namespace platform_ops_create
+{
+	bool create_data_dir(const std::filesystem::path& exe_file_path, const std::string& data_dir_name)
+	{
+		return std::filesystem::create_directories(exe_file_path / data_dir_name);
+		// exe_file_path is a filesystem::path type
+		// data_dir_name is implicityly casted from string to filesystem::path as well
+		// the / combine both in a full path
+	}
+
+} //end platform_ops_helper
